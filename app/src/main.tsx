43926b9bee4ca77389963f031d765d3804992195
YS-2357 <joungyoungsun20@gmail.com>
2025-12-30 13:10:44 +0900

    feat: 라우터 적용

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import AwardsPage from './components/AwardsPage.tsx'
+import MarkdownPage from './components/MarkdownPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/awards" element={<AwardsPage />} />
+        <Route path="/projects/codeit/:project/:page" element={<MarkdownPage />} />
+        <Route path="/projects/codeit/:project" element={<MarkdownPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
